dd55baf1c2a5810f29113ed59110e5e2d5bb3b8a
margaret153 <mrgrtgluhova@gmail.com>
2022-11-05 19:59:33 +0200

    version1.3

--- homework05.11.2022.py
+++ homework05.11.2022.py
@@ -6,10 +6,11 @@ class Human:
         self.job = job
         self.home = home
         self.car = car
-        self.money = 500
-        self.satiety = 100
-        self.gladness = 100
-        self.home.mess = 75
+        self.money = 100
+        self.satiety = 50
+        self.food = 70
+        self.gladness = 60
+
 
     def get_home(self):
         self.home = Home()
@@ -24,28 +25,30 @@ class Human:
         self.job = Job(job_list)
 
     def eat(self):
-        if self.home <= 0:
-            self.shopping('food')
+        if self.food <= 0:
+            self.eating('EAT')
         else:
             if self.satiety >= 100:
                 self.satiety = 100
                 return
-            self.satiety += 5
-            self.home.food -= 5
+            self.satienty += 6
+            self.home.food -= 6
+
+    def eat(self):
+        if self.home <= 0:
+            self.shopping('food')
+
     def work(self):
-        if self.car.drive():
+        if self.car.drive()
             pass
         else:
             if self.car.fuel < 20:
                 self.shopping('fuel')
                 return
-            else:
-                self.to_repair()
-                return
-        self.money += self.job.salary
-        self.gladness -= self.job.gladness_less
-        self.satiety -= 4
-    def shopping(self, manage):
+            self.money += self.job.salary
+            self.gladness -= self.job.gladness_less
+            self.satiety -= 4
+    def shopping(self,manage):
         if self.car.drive():
             pass
         else:
@@ -54,47 +57,35 @@ class Human:
             else:
                 self.to_repair()
                 return
-        if manage == 'fuel':
-            print('I bought fuel')
-            self.money -= 100
-            self.car.fuel += 100
-        elif manage == 'food':
-            print('Bought food')
-            self.money -= 50
-            self.home.food += 50
-        elif manage == 'delicacies':
-            print('I happy')
-            self.gladness += 10
-            self.money -= 15
-            self.satiety += 2
+            if manage == 'fuel' :
+                print('I bought fuel')
+                self.money -= 100
+                self.car.fuel += 100
+            elif manage == 'food':
+                print('Bought food')
+                self.money -= 50
+                self.home.food += 50
+            elif manage == 'delicacies' :
+                print('I happy')
+                self.gladness += 10
+                self.money -= 15
+                self.satienty += 2
 
 
     def chill(self):
         self.gladness += 10
-        self.home.mess += 75
+        self.home.mess += 5
     def clean_home(self):
         self.gladness -= 5
         self.home.mess = 0
-
-
-
-
-
     def to_repair(self):
-        self.car.strength += 100
+        self.car.strenght += 100
         self.money -= 50
 
     def days_indexes(self):
-        pass
-
-
-
 
     def is_alive(self):
         pass
-
-
-
     def live(self):
         pass
 
@@ -125,11 +116,11 @@ class Job:
         self.salary = job_list[self.job]['salary']
         self.gladness_less = job_list[self.job]['gladness_less']
 
-
 job_list = {'Java': {'salary': 50, 'gladness_less': 10},
             'Python': {'salary': 40, 'gladness_less': 3},
             'C++': {'salary': 70, 'gladness_less': 25},
             'Rust': {'salary': 60, 'gladness_less': 1}}
+
 
 brand_of_car = {'BMW': {'fuel': 100, 'strength': 100, 'consumption': 6},
                 'Lada': {'fuel': 50, 'strength': 40, 'consumption': 10},
